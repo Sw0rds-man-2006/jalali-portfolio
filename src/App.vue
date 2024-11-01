d59d7bc7e5e56654defe8c78443fd729eb0c3e46
mahdi <jalalimahdi866@gmail.com>
2024-11-02 00:20:58 +0330

    afte adding project

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,56 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import homeComp from "./components/home/home-comp.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+  />
+  <div
+    id="app"
+    class="min-h-screen flex items-center p-0px b-continer overflow-auto text-white p-4"
+  >
+    <homeComp />
+    <div class="route-content mx-auto">
+      <router-view />
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+::-webkit-scrollbar {
+  width: 7px;
+  background-color: #414141;
+  border-radius: 25px;
+}
+::-webkit-scrollbar-thumb {
+  width: 10px;
+  height: 10px;
+  background-color: rgb(255, 191, 0);
+  border-radius: 25px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.b-continer {
+  background-color: rgb(20, 20, 20);
+  padding: 0px !important;
+  margin: 0px !important;
 }
 
-nav {
+.route-content {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 100vh;
+  overflow-y: auto;
+  max-width: 1800px;
+  right: 5% !important;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+@media screen and (max-width: 1023px) {
+  .route-content {
+    height: 90vh;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  .b-continer {
+    display: grid;
   }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from "node:url";
-
+import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+    }),
+    UnoCSS(),
+  ],
   base: "/jalali-portfolio/",
   resolve: {
     alias: {
