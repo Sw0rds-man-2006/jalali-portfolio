52af1b063d97e525517ca0767ccbfbb22c1fa77b
mahdi <jalalimahdi866@gmail.com>
2024-11-07 21:19:05 +0330

    fix: fix base path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import about from "../views/about.vue";
 import home from "../views/home.vue";
 import blog from "../views/blog.vue";
 import contact from "../views/contact.vue";
 
 const router = createRouter({
-  history: createWebHistory("/portfolio/"),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
